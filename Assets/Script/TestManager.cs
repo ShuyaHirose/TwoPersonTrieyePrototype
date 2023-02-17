ffce2e60acfa6e26014c36f7e2dd29c9d9673011
DS1-KVPL2018\shuya hirose <hirose.shuya.ds@tut.jp>
2023-02-17 15:09:36 +0900

    コントローラーのAボタンを押すとランダムな別のオブジェクトが生成されるようにした

--- Assets/Script/TestManager.cs
+++ Assets/Script/TestManager.cs
@@ -7,7 +7,7 @@ using Random = UnityEngine.Random;
 public class TestManager : MonoBehaviour
 {
     //球体プレハブ
-    public GameObject Cube;
+    public GameObject Sphere;
     public GameObject Wall;
     //public GameObject Camera;
     private GameObject CopyCube;
@@ -53,195 +53,170 @@ public class TestManager : MonoBehaviour
     List<int> lev_set = new List<int>();
     int[] lev_num = new int[3];
     // Start is called before the first frame update
-    void Start()
+    void Update()
     {
-        //球体の数分リストに値を順に格納
-        int start = 0;
-        //球体全体の数を算出
-        int num = row * colum;
-        float num_float = (float)num;
-        for (int i = start; i <= num-1; i++)
+        if (OVRInput.GetDown(OVRInput.RawButton.A))
         {
-            numbers.Add(i);
-        }
+            //初期化
+            delete_obj();
 
-        //球体間の距離を設定
-        float ball_size = (float)l_size;
-        float bet_len = ball_size  + sphere_len;
-
-        //各難易度設定をランダムに割り当て
-        for (int i = start + 1; i <= 3; i++)
-        {
-            lev_set.Add(i);
-        }
-        for (int i = start; i < 3; i++)
-        {
-            int index = Random.Range(0, lev_set.Count);
-            int RNG = lev_set[index];
-            lev_set.RemoveAt(index);
-            lev_num[i] = RNG;
-        }
-
-        while (numbers.Count > 0)
-        {
-            //リストの中から値を抜き出し、その値をリストから削除
-            int index = Random.Range(0, numbers.Count);
-            int RNG = numbers[index];
-            numbers.RemoveAt(index);
-
-
-            //配列に番号順に格納するようなイメージ
-            int sum = 0;
-            int x=0;
-            int y=0;
-            for (x = 1; x < row + 1; x++)
+            //球体の数分リストに値を順に格納
+            int start = 0;
+            //球体全体の数を算出
+            int num = row * colum;
+            float num_float = (float)num;
+            for (int i = start; i <= num - 1; i++)
             {
+                numbers.Add(i);
+            }
 
-                for (y = 1; y < colum + 1; y++)
+            //球体間の距離を設定
+            float ball_size = (float)l_size;
+            float bet_len = ball_size + sphere_len;
+
+            //各難易度設定をランダムに割り当て
+            for (int i = start + 1; i <= 3; i++)
+            {
+                lev_set.Add(i);
+            }
+            for (int i = start; i < 3; i++)
+            {
+                int index = Random.Range(0, lev_set.Count);
+                int RNG = lev_set[index];
+                lev_set.RemoveAt(index);
+                lev_num[i] = RNG;
+            }
+
+            while (numbers.Count > 0)
+            {
+                //リストの中から値を抜き出し、その値をリストから削除
+                int index = Random.Range(0, numbers.Count);
+                int RNG = numbers[index];
+                numbers.RemoveAt(index);
+
+
+                //配列に番号順に格納するようなイメージ
+                int sum = 0;
+                int x = 0;
+                int y = 0;
+                for (x = 1; x < row + 1; x++)
                 {
-                    sum++;
+
+                    for (y = 1; y < colum + 1; y++)
+                    {
+                        sum++;
+                        if (RNG == sum)
+                        {
+                            break;
+                        }
+                    }
                     if (RNG == sum)
                     {
                         break;
                     }
                 }
-                if (RNG == sum)
+                if (x > row)
                 {
-                    break;
+                    x = row;
+                    y = colum;
                 }
-            }
-            if (x > row)
-            {
-                x = row;
-                y = colum;
-            }
 
-            //奥行きをランダムに生成
-            float rng_len = Random.Range(1, 4);
-            float z = rng_len * sphere_dep;
+                //奥行きをランダムに生成
+                float rng_len = Random.Range(1, 4);
+                float z = rng_len * sphere_dep;
 
-            //大きさをランダムに割り当て
-            int rng = 0;
-            int lev_size = 0;
-            rng = Random.Range(0, 101 + level);
-            if(rng  < 0)
-            {
-                while (rng < 0)
+                //大きさをランダムに割り当て
+                int rng = 0;
+                int lev_size = 0;
+                rng = Random.Range(0, 101 + level);
+                if (rng < 0)
                 {
-                    rng = Random.Range(0, 101 + level);
+                    while (rng < 0)
+                    {
+                        rng = Random.Range(0, 101 + level);
+                    }
                 }
+
+                if (rng < 34)
+                {
+                    lev_size = 1;
+                }
+                else if (33 < rng & rng < 67)
+                {
+                    lev_size = 2;
+                }
+                else if (66 < rng)
+                {
+                    lev_size = 3;
+                }
+                //球体のサイズを割り当て
+                if (lev_num[0] == lev_size)
+                {
+                    //球体をインスタンス化する(生成する)
+                    CreateCube(x, y, z, bet_len, l_size);
+
+                }
+                else if (lev_num[1] == lev_size)
+                {
+                    //球体をインスタンス化する(生成する)
+                    CreateCube(x, y, z, bet_len, n_size);
+                }
+                else if (lev_num[2] == lev_size)
+                {
+                    //球体をインスタンス化する(生成する)
+                    CreateCube(x, y, z, bet_len, s_size);
+                }
+
             }
 
-            Debug.Log(rng);
-            if (rng < 34)
-            {
-                lev_size = 1;
-            }
-            else if(33 < rng & rng <67)
-            {
-                lev_size = 2;
-            }
-            else if (66 < rng)
-            {
-                lev_size = 3;
-            }
-            //球体のサイズを割り当て
-            Debug.Log(lev_size);
-            if (lev_num[0] == lev_size)
-            {
-                //球体をインスタンス化する(生成する)
-                CreateCube(x, y, z, bet_len, l_size);
+            //球体群の中心座標を獲得しカメラを中心に移動させる
+            float Cam_x = (row + 1) * bet_len / 2;
+            float Cam_y = (colum + 1) * bet_len / 2;
 
-            }
-            else if (lev_num[1] == lev_size)
-            {
-                //球体をインスタンス化する(生成する)
-                CreateCube(x, y, z, bet_len, n_size);
-            }
-            else if (lev_num[2] == lev_size)
-            {
-                //球体をインスタンス化する(生成する)
-                CreateCube(x, y, z, bet_len, s_size);
-            }
-
+            //カメラの位置を変更
+            Vector3 tmp = GameObject.Find("OVRCameraRig").transform.position;
+            GameObject.Find("OVRCameraRig").transform.position = new Vector3(Cam_x, Cam_y, - cam_len * 10);
+            //Instantiate(Camera, new Vector3(Cam_x, Cam_y, -cam_len * 10), Quaternion.identity);
+            //壁の生成
+            float Wall_z = 4 * sphere_dep * bet_len;
+            Instantiate(Wall, new Vector3(Cam_x, Cam_y, Wall_z), Quaternion.Euler(-90, 0, 0));
         }
 
-        //球体群の中心座標を獲得しカメラを中心に移動させる
-        float Cam_x = (row + 1) * bet_len / 2;
-        float Cam_y = (colum + 1) * bet_len / 2;
-
-        //カメラの位置を変更
-        Vector3 tmp = GameObject.Find("OVRCameraRig").transform.position;
-        GameObject.Find("OVRCameraRig").transform.position = new Vector3(Cam_x,Cam_y, tmp.z - cam_len * 10);
-        //Instantiate(Camera, new Vector3(Cam_x, Cam_y, -cam_len * 10), Quaternion.identity);
-        //壁の生成
-        float Wall_z = 4 * sphere_dep * bet_len;
-        Instantiate(Wall, new Vector3(Cam_x, Cam_y, Wall_z), Quaternion.Euler(-90, 0, 0));
-
-        //大きさと座標をもとに球体を生成する
-        void CreateCube(int x,int y,float z,float len,float scale)
+        //オブジェクトの削除
+        void delete_obj()
         {
-            CopyCube = Instantiate(Cube,new Vector3(x * len, y * len, z * len), Quaternion.identity);
+            //初期化
+            GameObject[] objs = GameObject.FindGameObjectsWithTag("OBJs");
+
+            foreach (GameObject old_obj in objs)
+            {
+                Destroy(old_obj);
+            }
+        }
+        //大きさと座標をもとに球体を生成する
+        void CreateCube(int x, int y, float z, float len, float scale)
+        {
+            CopyCube = Instantiate(Sphere, new Vector3(x * len, y * len, z * len), Quaternion.identity);
             CopyCube.transform.localScale = new Vector3(scale, scale, scale);
         }
-
-        /*
-        //球体の大きさが均等になる
-        int[] ChangeLevel1(int SUM)
+    }
+}
+        /*void Update()
         {
-            int[] array = new int[3];
-            array[0] = SUM / 3;
-            array[1] = array[0] / 2;
-            array[2] = SUM - array[0] - array[1];
-
-            return array;
-        }
-        
-        //特定の大きさのみが5割、そのほかはランダム
-        int[] ChangeLevel2(float SUM,int sum)
-        {
-            int[] array = new int[3];
-            float r1 = Random.Range(0, 1);
-            float r2 = Random.Range(0, 1);
-            float r3 = Random.Range(0, 1);
-            float max_value =SUM * r1 * r2 / 2;
-            float ran_value = (SUM - max_value) * r3;
-            int fir_value = (int)Math.Floor(max_value);
-            int sec_value = (int)Math.Floor(ran_value);
-            int thi_value = sum - fir_value - sec_value;
-
-            array[0] = fir_value;
-            array[1] = sec_value;
-            array[2] = thi_value;
-
-            return array;
+            int a = 0;
+            if (OVRInput.GetDown(OVRInput.RawButton.A))
+            {
+            Vector3 tmp = GameObject.Find("OVRCameraRig").transform.position;
+            GameObject.Find("OVRCameraRig").transform.position = new Vector3(0, 0,0);
         }
 
-        //特定の大きさだけ極端に少なくなる
-        int[] ChangeLevel3(float SUM, int sum)
-        {
-            int[] array = new int[3];
-            float r1 = Random.Range(0, 1);
-            float r2 = Random.Range(0, 1);
-            float max_value = SUM * r1 * r1;
-            float ran_value = (SUM - max_value) * r2;
-            int fir_value = (int)Math.Floor(max_value);
-            int sec_value = (int)Math.Floor(ran_value);
-            int thi_value = sum - fir_value - sec_value;
-
-            array[0] = fir_value;
-            array[1] = sec_value;
-            array[2] = thi_value;
-
-            return array;
+            }
         }
         */
 
         //https://ics.media/entry/11292/
 
-    }
 
 
 
 
-}
